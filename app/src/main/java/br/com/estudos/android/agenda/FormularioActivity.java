4674b54ed4d76f08dd33b6e9ba75e1d192341338
Egila Karen <egilakaren@gmail.com>
2018-05-17 19:24:47 -0300

    Utilizando menus

--- app/src/main/java/br/com/estudos/android/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/estudos/android/agenda/FormularioActivity.java
@@ -1,7 +1,9 @@
 package br.com.estudos.android.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,7 +20,15 @@ public class FormularioActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(FormularioActivity.this, "Botao clicado", Toast.LENGTH_SHORT).show();
+
+                Intent vaiPraLista = new Intent(FormularioActivity.this, ListaAlunosActivity.class);
+                finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- app/src/main/java/br/com/estudos/android/agenda/ListaAlunosActivity.java
+++ app/src/main/java/br/com/estudos/android/agenda/ListaAlunosActivity.java
@@ -1,8 +1,11 @@
 package br.com.estudos.android.agenda;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 
@@ -17,5 +20,14 @@ public class ListaAlunosActivity extends AppCompatActivity {
         ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1 , alunos);
         listaAlunos.setAdapter(adapter);
+
+        Button botaoNovoAluno = (Button) findViewById(R.id.novo_aluno);
+        botaoNovoAluno.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentVaiProFormulario = new Intent(ListaAlunosActivity.this, FormularioActivity.class);
+                startActivity(intentVaiProFormulario);
+            }
+        });
     }
 }
